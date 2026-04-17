1bb63cf6df0b81bf1e0af4c54121d5a2f183e7fe
Adriana Tapia <tapiaadrianalucia@gmail.com>
2026-04-17 00:44:44 -0500

    Estructura inicial del backend e-learning

---
+++ src/test/java/com/elearning/platform_backend/PlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearning.platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
