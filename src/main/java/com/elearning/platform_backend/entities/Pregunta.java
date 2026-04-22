4835630ec75593a19e326da7301888779ec1c4fd
Adriana Tapia <tapiaadrianalucia@gmail.com>
2026-04-22 15:37:30 -0500

    feat: entidades JPA y relaciones del modelo

---
+++ src/main/java/com/elearning/platform_backend/entities/Pregunta.java
@@ -0,0 +1,33 @@
+package com.elearning.platform_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "pregunta")
+@Data
+@NoArgsConstructor
+public class Pregunta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String enunciado;
+
+    private String opciones; // puedes guardar como texto o JSON
+
+    private Integer valorPunto;
+
+    private String respuestaCorrecta;
+
+    @ManyToOne
+    @JoinColumn(name = "evaluacion_id")
+    private Evaluacion evaluacion;
+}
